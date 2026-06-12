c34cb9c200451940ed501711442a20bf84668d17
NepSauce <greasyandmoist@gmail.com>
2026-06-11 22:11:43 -0300

    Feat: Implement CSVParser class with unloadCSV method

--- include/csv_utils/csv_parser.hpp
+++ include/csv_utils/csv_parser.hpp
@@ -1,5 +1,12 @@
 #ifndef CSV_PARSER_HPP
 #define CSV_PARSER_HPP
 
+#include <string>
 
+class CSVParser {
+    private:
+        int* unloadCSV();
+    public:
+        CSVParser(std::string )
+};
 #endif
