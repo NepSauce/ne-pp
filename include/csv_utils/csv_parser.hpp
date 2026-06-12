af21b88af758a776e107d080b1e45588a667da09
NepSauce <zawadatif.builds@gmail.com>
2026-06-11 22:30:30 -0300

    Feat: Update CSVParser constructor

--- include/csv_utils/csv_parser.hpp
+++ include/csv_utils/csv_parser.hpp
@@ -2,11 +2,19 @@
 #define CSV_PARSER_HPP
 
 #include <string>
+#include "../structures/file_struct.hpp"
+
 
 class CSVParser {
     private:
+        std::string filePath;
+        bool header;
+        char delimiter;
+
         int* unloadCSV();
     public:
-        CSVParser(std::string )
+        CSVParser(std::string filePath, bool header, char delimiter);
+        std::unique_ptr<DataFile> dataFrame();
 };
+
 #endif
