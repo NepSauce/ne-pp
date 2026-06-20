36fb8e380da1908f59a2ac75363622b5625b5ff1
NepSauce <zawadatif.builds@gmail.com>
2026-06-20 14:57:46 -0300

    Feat: Add string trimming tests in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include "../include/ne_pp/pp/utils/token_utils/string_pp.hpp"
+
 int main() {
+    std::string testStr = "  six seven  ";
+    std::cout << ne_pp::pp::StringPP(testStr).trimLeft().toString() << std::endl;    
+    std::cout << ne_pp::pp::StringPP(testStr).trimRight().toString() << std::endl;
+    std::cout << ne_pp::pp::StringPP(testStr).trim().toString() << std::endl;
+    
     return 0;
 }
