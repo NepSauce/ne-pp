0634e4a892545e2db3e6265c4676541bcc75c2ae
NepSauce <zawadatif.builds@gmail.com>
2026-06-23 22:47:39 -0300

    Feat: Add CSV parsing functionality to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "../include/ne_pp/pp/utils/token_utils/string_pp.hpp"
+#include <ne_pp/pp/utils/csv_utils/csv_parser.hpp>
 
 int main() {
     std::string testStr = "  six seven  ";
@@ -8,5 +9,21 @@ int main() {
     std::cout << ne_pp::pp::StringPP(testStr).trimRight().toString() << std::endl;
     std::cout << ne_pp::pp::StringPP(testStr).trim().toString() << std::endl;
     
+    ne_pp::pp::CSVParser parser("../corpus/csv/comma_delimiter.csv", true, ',');
+    std::unique_ptr<ne_pp::pp::CSVDataFile> myFrame = parser.dataFrame();
+
+    for (size_t j = 0; j < myFrame->columns.size(); ++j) {
+        const auto& col = myFrame->columns[j];
+        std::cout << col.header << " (Nulls: " << col.nullPosition.size() << ")\n";
+    }
+    size_t totalRows = myFrame->columns[0].data.size();
+
+    for (size_t i = 0; i < totalRows; ++i) {
+        for (size_t j = 0; j < myFrame->columns.size(); ++j) {
+            std::cout << myFrame->columns[j].data[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
     return 0;
 }
